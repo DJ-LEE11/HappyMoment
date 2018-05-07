7da20f33d770577956c03650991e507f1c6c1052
lidongjie <18825098496@163.com>
2018-05-07 11:58:34 +0800

    1.multiDex分包。 2.添加签名。

---
+++ app/src/main/java/com/uwork/happymoment/App.java
@@ -0,0 +1,16 @@
+package com.uwork.happymoment;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * @author 李栋杰
+ * @time 2018/5/7  上午11:36
+ * @desc ${TODD}
+ */
+public class App extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/uwork/happymoment/mvp/MainActivity.java
+++ app/src/main/java/com/uwork/happymoment/mvp/MainActivity.java
@@ -1,9 +1,12 @@
-package com.uwork.happymoment;
+package com.uwork.happymoment.mvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uwork.happymoment.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
